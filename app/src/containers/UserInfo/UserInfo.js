54298eb63cee827986e8660ae08dc39fbaa19235
Ahtazaz Ahmed Khan <ahmedahtazaz@gmail.com>
2021-04-27 01:36:16 +0500

    Datepicker bug fix.

--- app/src/containers/UserInfo/UserInfo.js
+++ app/src/containers/UserInfo/UserInfo.js
@@ -57,116 +57,122 @@ function UserInfo({loader}) {
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
+    setShowCalender(false);
     if (day) setDob(day + '-' + month + '-' + year);
     if (day) setCalDate(date);
-    setShowCalender(false);
   };
 
   return (
-    <ScrollView style={{height:'100%'}}>
-    <View style={styles.background}>
-      <View style={styles.innerDiv}>
-        <View style={styles.mainHeading}>
-          <Text style={styles.mainHeadingText}>
-            {isFamily ? 'Add Family' : 'User Information'}
-          </Text>
-        </View>
-        <View style={styles.smallHeading}>
-          <Text style={styles.smallHeadingText}>
-            {isFamily
-              ? 'Please Information of Family Member'
-              : 'Please provide your information to continue'}
-          </Text>
-        </View>
-        <View style={styles.secondaryHeading}>
-          <Text style={styles.secondaryHeadingText}>User Information</Text>
-        </View>
-        
-        <View style={styles.formContainer}>
-          <View style={styles.userName}>
-            <TextInput
-              value={fName}
-              textContentType="givenName"
-              underlineColorAndroid="transparent"
-              placeholder="First Name"
-              style={styles.inputStyle}
-              onChangeText={value => setFName(value)}></TextInput>
-            <TextInput
-              value={lName}
-              textContentType="familyName"
-              placeholder="Last Name"
-              style={styles.inputStyle}
-              onChangeText={value => setLName(value)}></TextInput>
+    <ScrollView style={{height: '100%'}}>
+      <View style={styles.background}>
+        <View style={styles.innerDiv}>
+          <View style={styles.mainHeading}>
+            <Text style={styles.mainHeadingText}>
+              {isFamily ? 'Add Family' : 'User Information'}
+            </Text>
           </View>
-          <View style={styles.gender}>
-            <RadioButton
-              checked={male}
-              data="Male"
-              setChecked={value => {
-                if (value) {
-                  setFemale(false);
-                  setOther(false);
-                }
-                setMale(value);
-              }}
-              widthFactorMain="25"></RadioButton>
-            <RadioButton
-              checked={female}
-              data="FeMale"
-              setChecked={value => {
-                if (value) {
-                  setMale(false);
-                  setOther(false);
-                }
-                setFemale(value);
-              }}
-              widthFactorMain="25"></RadioButton>
-            <RadioButton
-              checked={other}
-              data="Other"
-              setChecked={value => {
-                if (value) {
-                  setMale(false);
-                  setFemale(false);
-                }
-                setOther(value);
-              }}
-              widthFactorMain="25"></RadioButton>
+          <View style={styles.smallHeading}>
+            <Text style={styles.smallHeadingText}>
+              {isFamily
+                ? 'Please Information of Family Member'
+                : 'Please provide your information to continue'}
+            </Text>
           </View>
-         
-          <DropDownPicker
-            items={[
-              {
-                label: 'Bavaria',
-                value: 'Bavaria',
-              },
-              {
-                label: 'Berlin',
-                value: 'Berlin',
-              },
-              {
-                label: 'Munich',
-                value: 'Munich',
-              },
-            ]}
-            defaultValue={city}
-            containerStyle={{height: '15%'}}
-            style={{backgroundColor: '#F5F9F8', fontSize: RFValue(14, 580),
-            color: '#243E3B'}}
-            itemStyle={{
-              justifyContent: 'flex-start',
-            }}
-            dropDownStyle={{backgroundColor: '#F5F9F8', fontSize: RFValue(14, 580),
-            color: '#243E3B'}}
-            onChangeItem={item => setCity(item.value)}
-          />
-        </View>
-        <TextInput
+          <View style={styles.secondaryHeading}>
+            <Text style={styles.secondaryHeadingText}>User Information</Text>
+          </View>
+
+          <View style={styles.formContainer}>
+            <View style={styles.userName}>
+              <TextInput
+                value={fName}
+                textContentType="givenName"
+                underlineColorAndroid="transparent"
+                placeholder="First Name"
+                style={styles.inputStyle}
+                onChangeText={value => setFName(value)}></TextInput>
+              <TextInput
+                value={lName}
+                textContentType="familyName"
+                placeholder="Last Name"
+                style={styles.inputStyle}
+                onChangeText={value => setLName(value)}></TextInput>
+            </View>
+            <View style={styles.gender}>
+              <RadioButton
+                checked={male}
+                data="Male"
+                setChecked={value => {
+                  if (value) {
+                    setFemale(false);
+                    setOther(false);
+                  }
+                  setMale(value);
+                }}
+                widthFactorMain="25"></RadioButton>
+              <RadioButton
+                checked={female}
+                data="FeMale"
+                setChecked={value => {
+                  if (value) {
+                    setMale(false);
+                    setOther(false);
+                  }
+                  setFemale(value);
+                }}
+                widthFactorMain="25"></RadioButton>
+              <RadioButton
+                checked={other}
+                data="Other"
+                setChecked={value => {
+                  if (value) {
+                    setMale(false);
+                    setFemale(false);
+                  }
+                  setOther(value);
+                }}
+                widthFactorMain="25"></RadioButton>
+            </View>
+
+            <DropDownPicker
+              items={[
+                {
+                  label: 'Bavaria',
+                  value: 'Bavaria',
+                },
+                {
+                  label: 'Berlin',
+                  value: 'Berlin',
+                },
+                {
+                  label: 'Munich',
+                  value: 'Munich',
+                },
+              ]}
+              defaultValue={city}
+              containerStyle={{height: '15%'}}
+              style={{
+                backgroundColor: '#F5F9F8',
+                fontSize: RFValue(14, 580),
+                color: '#243E3B',
+              }}
+              itemStyle={{
+                justifyContent: 'flex-start',
+              }}
+              dropDownStyle={{
+                backgroundColor: '#F5F9F8',
+                fontSize: RFValue(14, 580),
+                color: '#243E3B',
+              }}
+              onChangeItem={item => setCity(item.value)}
+            />
+          </View>
+          <TextInput
             value={dob}
             textContentType="none"
             placeholder="Date"
             style={styles.inputStyle1}
-            onFocus={() => {
+            onPressIn={() => {
               setShowCalender(true);
             }}></TextInput>
           {showCalender ? (
@@ -180,30 +186,30 @@ function UserInfo({loader}) {
             />
           ) : null}
           <TextInput
-              value={taxId}
-              textContentType="taxId"
-              underlineColorAndroid="transparent"
-              placeholder="Tax ID"
-              style={styles.inputStyle1}
-              onChangeText={value => setTaxId(value)}></TextInput>
+            value={taxId}
+            textContentType="taxId"
+            underlineColorAndroid="transparent"
+            placeholder="Tax ID"
+            style={styles.inputStyle1}
+            onChangeText={value => setTaxId(value)}></TextInput>
           <TextInput
-              value={email}
-              textContentType="email"
-              underlineColorAndroid="transparent"
-              placeholder="Email"
-              style={styles.inputStyle1}
-              onChangeText={value => setEmail(value)}></TextInput>
-            <TextInput
-              value={mobileNo}
-              textContentType="mobileNo"
-              underlineColorAndroid="transparent"
-              placeholder="Mobile No"
-              style={styles.inputStyle1}
-              onChangeText={value => setMobileNo(value)}></TextInput>
-            <View style={styles.secondaryHeading}>
-              <Text style={styles.secondaryHeadingText}>Address</Text>
-            </View>
-            <View style={styles.userName}>
+            value={email}
+            textContentType="email"
+            underlineColorAndroid="transparent"
+            placeholder="Email"
+            style={styles.inputStyle1}
+            onChangeText={value => setEmail(value)}></TextInput>
+          <TextInput
+            value={mobileNo}
+            textContentType="mobileNo"
+            underlineColorAndroid="transparent"
+            placeholder="Mobile No"
+            style={styles.inputStyle1}
+            onChangeText={value => setMobileNo(value)}></TextInput>
+          <View style={styles.secondaryHeading}>
+            <Text style={styles.secondaryHeadingText}>Address</Text>
+          </View>
+          <View style={styles.userName}>
             <TextInput
               value={schiller}
               textContentType="schiller"
@@ -235,44 +241,49 @@ function UserInfo({loader}) {
             ]}
             defaultValue={city1}
             containerStyle={{height: '15%'}}
-            style={{backgroundColor: '#F5F9F8', fontSize: RFValue(14, 580),
-            color: '#243E3B'}}
+            style={{
+              backgroundColor: '#F5F9F8',
+              fontSize: RFValue(14, 580),
+              color: '#243E3B',
+            }}
             itemStyle={{
               justifyContent: 'flex-start',
             }}
-            dropDownStyle={{backgroundColor: '#F5F9F8', fontSize: RFValue(14, 580),
-            color: '#243E3B'}}
+            dropDownStyle={{
+              backgroundColor: '#F5F9F8',
+              fontSize: RFValue(14, 580),
+              color: '#243E3B',
+            }}
             onChangeItem={item => setCity1(item.value)}
           />
           <TextInput
-              value={fName}
-              textContentType="postalCode"
-              underlineColorAndroid="transparent"
-              placeholder="Postal Code"
-              style={styles.inputStyle1}
-              onChangeText={value => setPostalCode(value)}></TextInput>
-              <TouchableOpacity
-            style={[styles.container, styles.submitButtonDark]}>
+            value={fName}
+            textContentType="postalCode"
+            underlineColorAndroid="transparent"
+            placeholder="Postal Code"
+            style={styles.inputStyle1}
+            onChangeText={value => setPostalCode(value)}></TextInput>
+          <TouchableOpacity style={[styles.container, styles.submitButtonDark]}>
             <Text style={styles.submitText}>Save {'\u2B24'} Continue</Text>
           </TouchableOpacity>
           <View style={styles.saveCloseDiv}>
-              <Text style={styles.saveCloseText}>Save & add another member</Text>
-            </View>
-        {loader ? (
-          <View
-            style={{
-              alignSelf: 'center',
-              height: '100%',
-              width: '100%',
-              justifyContent: 'center',
-              position: 'absolute',
-              zIndex: 1000,
-            }}>
-            <ActivityIndicator size="large" color="grey" animating={loader} />
+            <Text style={styles.saveCloseText}>Save & add another member</Text>
           </View>
-        ) : null}
+          {loader ? (
+            <View
+              style={{
+                alignSelf: 'center',
+                height: '100%',
+                width: '100%',
+                justifyContent: 'center',
+                position: 'absolute',
+                zIndex: 1000,
+              }}>
+              <ActivityIndicator size="large" color="grey" animating={loader} />
+            </View>
+          ) : null}
+        </View>
       </View>
-    </View>
     </ScrollView>
   );
 }
@@ -296,7 +307,7 @@ const styles = StyleSheet.create({
     paddingLeft: '5%',
     paddingRight: '5%',
   },
- 
+
   mainHeadingText: {
     fontSize: RFValue(16, 580),
     color: PRIMARY_COLOR,
@@ -320,8 +331,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'row',
     marginBottom: 4,
-    width:'100%',
-    
+    width: '100%',
   },
   inputStyle: {
     display: 'flex',
@@ -338,7 +348,7 @@ const styles = StyleSheet.create({
   },
   inputStyle1: {
     display: 'flex',
-   
+
     backgroundColor: '#F5F9F8',
     borderRadius: 6,
     fontSize: RFValue(14, 580),
@@ -382,12 +392,12 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     paddingBottom: 15,
     fontSize: RFValue(14, 580),
-    fontWeight:'600',
+    fontWeight: '600',
   },
-  saveCloseText : {
+  saveCloseText: {
     fontSize: RFValue(14, 580),
-    fontWeight:'600',
+    fontWeight: '600',
     color: '#212826',
-    marginBottom:180,
-  }
+    marginBottom: 180,
+  },
 });
