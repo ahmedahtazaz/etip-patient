be1f9ab499c6ef38e92b112672a257d785f2a093
naveed350 <naveed350@gmail.com>
2021-05-18 07:38:09 +0200

    welcome screen button bottom margin

--- app/src/containers/Welcome/Welcome.js
+++ app/src/containers/Welcome/Welcome.js
@@ -242,7 +242,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    height: '72%',
+    height: '71%',
   },
   welcomeBottomText: {
     display: 'flex',
